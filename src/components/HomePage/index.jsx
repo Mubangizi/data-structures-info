2b1886941fdc12218da66ed28e4b2348ce0a7443
mubangizi <mubangizia22@gmail.com>
2021-04-05 14:55:44 +0300

    Cleanup to remove default react data

---
+++ src/components/HomePage/index.jsx
@@ -0,0 +1,21 @@
+import "./HomePage.css";
+
+function App() {
+  return (
+    <div className="Home">
+      <header className="HomeHeader">
+        <h1>Data Structures</h1>
+        <p>
+          <code>A data structure </code>
+          is a named location that can be used to store and organize data.
+        </p>
+        <p>
+          <code>An algorithm</code> is a collection of steps to solve a
+          particular problem.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
