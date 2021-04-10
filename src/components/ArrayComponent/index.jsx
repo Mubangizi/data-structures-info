756cad58d7511e6595c1ba5a19033dea6af8ac42
mubangizi <mubangizia22@gmail.com>
2021-04-11 00:09:33 +0300

    Add Array Info

--- src/components/ArrayComponent/index.jsx
+++ src/components/ArrayComponent/index.jsx
@@ -3,7 +3,7 @@ import "./ArrayComponent.css";
 const ArrayComponent = () => {
   return (
     <div className="ArrayComponent">
-      <h2 className="ItemHeading">Array</h2>
+      <h1 className="ItemHeading">Array</h1>
       <div className="ItemDescription">
         Array is a container which can hold a fix number of items and these
         items should be of the same type. Most of the data structures make use
@@ -15,6 +15,17 @@ const ArrayComponent = () => {
             Index − Each location of an element in an array has a numerical
             index, which is used to identify the element.
           </li>
+        </ul>
+        <h3>Basic Operations</h3>
+        Following are the basic operations supported by an array.
+        <ul>
+          <li>Traverse − print all the array elements one by one.</li>
+          <li>Insertion − Adds an element at the given index.</li>
+          <li>Deletion − Deletes an element at the given index.</li>
+          <li>
+            Search − Searches an element using the given index or by the value.
+          </li>
+          <li>Update − Updates an element at the given index.</li>
         </ul>
       </div>
     </div>
