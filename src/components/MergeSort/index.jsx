66a6cc74fee58f1b8b3b653253c1417334822080
mubangizi <mubangizia22@gmail.com>
2021-04-12 00:43:38 +0300

    Add Merge sort demo

--- src/components/MergeSort/index.jsx
+++ src/components/MergeSort/index.jsx
@@ -26,27 +26,35 @@ const MergeSort = () => {
     setSorted([]);
   };
 
+  const merge = (left, right) => {
+    let arr = [];
+    // Break out of loop if any one of the array gets empty
+    while (left.length && right.length) {
+      // Pick the smaller among the smallest element of left and right sub arrays
+      if (left[0] < right[0]) {
+        arr.push(left.shift());
+      } else {
+        arr.push(right.shift());
+      }
+    }
+    // Concatenating the leftover elements
+    return [...arr, ...left, ...right];
+  };
+
+  function mergeSort(array) {
+    const half = array.length / 2;
+    // Base case or terminating case
+    if (array.length < 2) {
+      return array;
+    }
+    const left = array.splice(0, half);
+    return merge(mergeSort(left), mergeSort(array));
+  }
+
   const sortItemList = (e) => {
     e.preventDefault();
     let newArray = [...itemList];
-    let swapped;
-    const n = newArray.length;
-    for (let i = 0; i < n - 1; i++) {
-      swapped = false;
-      for (let j = 0; j < n - i - 1; j++) {
-        if (newArray[j] > newArray[j + 1]) {
-          const temp = newArray[j];
-          newArray[j] = newArray[j + 1];
-          newArray[j + 1] = temp;
-          swapped = true;
-        }
-      }
-      if (swapped === false) {
-        break;
-      }
-    }
-
-    setSorted(newArray);
+    setSorted(mergeSort(newArray));
   };
 
   return (
